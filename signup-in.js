3c15a0ec7570c557e2efe690c9f55a44c6222018
Abdallah hijazi <hijaziboudi0@gmail.com>
2023-05-20 17:21:35 +0300

    add some comments and notes

--- signup-in.js
+++ signup-in.js
@@ -1,4 +1,4 @@
-//hon bas 3am jib lcss te3 klchi bedi esta3emlu
+//hon bas 3am jib lcss w7eton b2lab variable kermel 2e2der esta3melon
 let switchCtn = document.querySelector("#switch-cnt");
 let switchC1 = document.querySelector("#switch-c1");
 let switchC2 = document.querySelector("#switch-c2");
@@ -8,30 +8,38 @@ let aContainer = document.querySelector("#a-container");
 let bContainer = document.querySelector("#b-container");
 let allButtons = document.querySelectorAll(".submit");
 
-let getButtons = (e) => e.preventDefault(); // hon he ekhtisar la function eza de3ti fiya oulili bsir bekteba bchakla ltabi3i
+//he function esma getButtons 3enda parametre (e) wbtemna3 levent lal buttons
+let getButtons = (e) => e.preventDefault();
 
-let changeForm = (e) => {  //same hon bas hon fi kaza cha8le b2alb lfunction fa fta7et { }
-	switchCtn.classList.add("is-gx");// hon bas kermel ya3mel animation 
+//he function esma changeForm 3enda parametre (e)
+let changeForm = (e) => {  
+	// he bt7ot lcss (is-gx) yali khasu bl animation 3and ltag html li 3endu id (switch-cnt)
+	switchCtn.classList.add("is-gx");
+	// he kermel bas tekhlas lanimation yerja3 ychil lcss (is-gx) [lwa2et hewe 1500 ms]
 	setTimeout(function () { 
 		switchCtn.classList.remove("is-gx");
-	}, 1500);// he kela bas kermel elu ywa2ef ba3d 1.5s kermel may3id aw ysatel 
+	}, 1500);
 
-	switchCtn.classList.toggle("is-txr");//hon bas kermel khali ybadel bed left w right 
-	switchCircle[0].classList.toggle("is-txr");
-	switchCircle[1].classList.toggle("is-txr");
+	switchCtn.classList.toggle("is-txr"); //he kermel t7ot aw tchile lcss (is-txr) [7asab eza kan mawjoud bltag aw la] 3and lhtml tag li 3endu id (switch-cnt)
+	switchCircle[0].classList.toggle("is-txr");// he mtl li fo2 bas hal mara 3and ltag te3 lcircle 
+	switchCircle[1].classList.toggle("is-txr");// mtl li fo2 bas la2an 3ena 2 circle
 
-	switchC1.classList.toggle("is-hidden");//hon kermel maykoun kelchi fo2 ba3d fa wahde btedwi w wahde btetfi
-	switchC2.classList.toggle("is-hidden");
-	aContainer.classList.toggle("is-txl");//hon kermel left w right
-	bContainer.classList.toggle("is-txl");
-	bContainer.classList.toggle("is-z200");//bas kermel ghayer bl position 
+	switchC1.classList.toggle("is-hidden");//he kermel t7ot aw tchile lcss (is-hidden)
+	switchC2.classList.toggle("is-hidden");// same 
+	aContainer.classList.toggle("is-txl");// he kermel t7ot aw tchile lcss (is-txl) 3and lcontainer te3it l signup
+	bContainer.classList.toggle("is-txl");// mtl li fo2 bas hal mara lal container te3it l signin
+	bContainer.classList.toggle("is-z200"); // he mtl li fo2 bas lcss (is-z200) badal lcss (is-txl)
 };
 
-let mainF = (e) => { //kamen hon e5tisar la function bas b2alba 2 for 3am yesta3emlu lfunction li fo2 
+//he function esma mainF 3enda parametre (e)
+let mainF = (e) => {  
+	//he lfor kermel tchuf kam button 3ena wt7etelon lfunction getButtons kermel ma 3ida la kl button
 	for (var i = 0; i < allButtons.length; i++)
 		allButtons[i].addEventListener("click", getButtons);
+	//he kermel tchuf lbutton li khason bltebdil wl animation wt7eteln el lfunction changeForm	kermel ma 3ida la kl button
 	for (var i = 0; i < switchBtn.length; i++)
 		switchBtn[i].addEventListener("click", changeForm);
 };
 
-window.addEventListener("load", mainF);//hon bas kermel lfunction tkoun che8ale de8ri awal mayfut 3al page 
+//he kermel lfunction mainF techte8el awal ma na3mel load lal page
+window.addEventListener("load", mainF);
--- signup-r.js
+++ signup-r.js
@@ -1,4 +1,6 @@
-//nafsu zetu te3 signup-in bas 3melet copy la2an yemken nzid hon kan function zyede wyemken ghayer chway
+//nafsu zetu te3 signup-in.js bas 3melet copy la2an yemken nzid hon kam function wyemken ghayer blmawjoudin chway chway
+
+//hon bas 3am jib lcss w7eton b2lab variable kermel 2e2der esta3melon
 let switchCtn = document.querySelector("#switch-cnt");
 let switchC1 = document.querySelector("#switch-c1");
 let switchC2 = document.querySelector("#switch-c2");
@@ -8,30 +10,38 @@ let aContainer = document.querySelector("#a-container");
 let bContainer = document.querySelector("#b-container");
 let allButtons = document.querySelectorAll(".submit");
 
+//he function esma getButtons 3enda parametre (e) wbtemna3 levent lal buttons
 let getButtons = (e) => e.preventDefault();
 
+//he function esma changeForm 3enda parametre (e)
 let changeForm = (e) => {
+	// he bt7ot lcss (is-gx) yali khasu bl animation 3and ltag html li 3endu id (switch-cnt)
 	switchCtn.classList.add("is-gx");
-	setTimeout(function () {
+	// he kermel bas tekhlas lanimation yerja3 ychil lcss (is-gx) [lwa2et hewe 1500 ms]
+	setTimeout(function () { 
 		switchCtn.classList.remove("is-gx");
 	}, 1500);
 
-	switchCtn.classList.toggle("is-txr");
-	switchCircle[0].classList.toggle("is-txr");
-	switchCircle[1].classList.toggle("is-txr");
+	switchCtn.classList.toggle("is-txr");//he kermel t7ot aw tchile lcss (is-txr) [7asab eza kan mawjoud bltag aw la] 3and lhtml tag li 3endu id (switch-cnt)
+	switchCircle[0].classList.toggle("is-txr");// he mtl li fo2 bas hal mara 3and ltag te3 lcircle 
+	switchCircle[1].classList.toggle("is-txr");// mtl li fo2 bas la2an 3ena 2 circle
 
-	switchC1.classList.toggle("is-hidden");
-	switchC2.classList.toggle("is-hidden");
-	aContainer.classList.toggle("is-txl");
-	bContainer.classList.toggle("is-txl");
-	bContainer.classList.toggle("is-z200");
+	switchC1.classList.toggle("is-hidden");//he kermel t7ot aw tchile lcss (is-hidden)
+	switchC2.classList.toggle("is-hidden");// same 
+	aContainer.classList.toggle("is-txl");// he kermel t7ot aw tchile lcss (is-txl) 3and lcontainer te3it l BMI
+	bContainer.classList.toggle("is-txl");// mtl li fo2 bas hal mara lal container te3it l medical history etc
+	bContainer.classList.toggle("is-z200");// he mtl li fo2 bas lcss (is-z200) badal lcss (is-txl)
 };
 
+//he function esma mainF 3enda parametre (e)
 let mainF = (e) => {
+	//he lfor kermel tchuf kam button 3ena wt7etelon lfunction getButtons kermel ma 3ida la kl button
 	for (var i = 0; i < allButtons.length; i++)
 		allButtons[i].addEventListener("click", getButtons);
+	//he kermel tchuf lbutton li khason bltebdil wl animation wt7eteln el lfunction changeForm	kermel ma 3ida la kl button
 	for (var i = 0; i < switchBtn.length; i++)
 		switchBtn[i].addEventListener("click", changeForm);
 };
 
+//he kermel lfunction mainF techte8el awal ma na3mel load lal page
 window.addEventListener("load", mainF);
